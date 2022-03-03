0b1203c54d9e6ef327d0500aa5a68356ccf31849
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-03-03 13:09:24 +0100

    Added topics: transactions, anomalies & normals

---
+++ streaming_kafka/predictive_maintenance.py
@@ -0,0 +1,43 @@
+import os
+import json
+import logging
+import numpy as np
+from joblib import load
+from multiprocessing import Process
+from streaming.utils import create_producer, create_consumer
+from settings import TRANSACTIONS_TOPIC, TRANSACTIONS_CONSUMER_GROUP, ANOMALIES_TOPIC, NUM_PARTITIONS
+
+def detect():
+    consumer = create_consumer(topic=TRANSACTIONS_TOPIC, group_id=TRANSACTIONS_CONSUMER_GROUP)
+    producer = create_producer()
+
+    while True:
+        message = consumer.poll(timeout=50)
+        if message is None:
+            continue
+            
+        if message.error():
+            logging.error("Consumer error: {}".format(message.error()))
+            continue
+
+        # Message that came from producer
+        record = json.loads(message.value().decode('utf-8'))
+        data = record["data"]
+
+        prediction = clf.predict(data)
+
+        # If an anomaly comes in, send it to anomalies topic
+        if prediction[0] == -1:
+            score = clf.score_samples(data)
+            record["score"] = np.round(score, 3).tolist()
+
+            _id = str(record["id"])
+            record = json.dumps(record).encode("utf-8")
+
+            producer.produce(topic=ANOMALIES_TOPIC,
+                             value=record)
+            producer.flush()
+
+        # consumer.commit() # Uncomment to process all messages, not just new ones
+
+    consumer.close()
